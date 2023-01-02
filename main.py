77eecdbf280fa3f3f7ad27dd80b8840cfeb97ab7
Anderson Lopes <anderson_lops@hotmail.com>
2023-01-01 22:05:37 -0300

    :hammer: Update Readme

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 from reportlab.pdfgen import canvas
 
-def GeneratePDF():
+def sizeSpace(marca):
+  tamanho = len(marca);
+  print(tamanho)
+
+def GeneratePDF(lista):
     try:
-        nome_pdf = "teste01"
+        nome_pdf = 'teste01'
         pdf = canvas.Canvas('{}.pdf'.format(nome_pdf))
 
         pdf.setFont("Helvetica-Bold", 70)
         pdf.drawString(100, 600, "CERVEJA")
         pdf.setTitle(nome_pdf)
         pdf.setFont("Helvetica", 30)
-        pdf.drawString(100, 560, "SKOL")
+        pdf.drawString(100,560, 'BAUDUCO')
         pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(260, 560, "300ML")
+        pdf.drawString(260,560, '300ML')
         pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(120, 380, "R$")
+        pdf.drawString(120,380, 'R$')
         pdf.setFont("Helvetica-Bold", 150)
-        pdf.drawString(180, 360, "50")
+        pdf.drawString(180,360, '50')
         pdf.setFont("Helvetica-Bold", 80)
-        pdf.drawString(350, 410, ",49")
+        pdf.drawString(350,410, ',49')
         pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(470, 350, "UND")
+        pdf.drawString(470,350, 'UND')
 
         pdf.rotate(15)
         pdf.rect(300, 250, 250, 4, fill=True)
         pdf.rect(340, 230, 185, 4, fill=True)
 
         pdf.save()
-        print('{}.pf criado com sucesso!'.format(nome_pdf) )
+        print('{}.pdf criado com sucesso!'.format(nome_pdf))
     except:
-        print('erro ao gerar {}.pdf'.format(nome_pdf))
+        print('Erro ao gerar {}.pdf'.format(nome_pdf))
 
-GeneratePDF()            
+lista = {'Rafaela': '19', 'Jose': '15', 'Maria': '22','Eduardo':'24'}
 
-        
+GeneratePDF(lista)
+# sizeSpace("skol")
