8c2aca8d4eaa452a2eebc864e086475b9217d8cd
Anderson Lopes <anderson_lops@hotmail.com>
2023-01-02 21:19:43 -0300

    :hammer: Update layout of page A4

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
 from reportlab.pdfgen import canvas
 
-def sizeSpace(marca):
-  tamanho = len(marca);
-  print(tamanho)
-
-def GeneratePDF(lista):
+def GeneratePDFA4(lista):
     try:
-        nome_pdf = 'teste01'
+        nome_pdf = lista["codigo"]
         pdf = canvas.Canvas('{}.pdf'.format(nome_pdf))
 
-        pdf.setFont("Helvetica-Bold", 70)
-        pdf.drawString(100, 600, "CERVEJA")
+        if(len(lista["nome"]) > 10):
+          pdf.setFont("Helvetica-Bold", 40)
+          pdf.drawString(40, 600, lista["nome"].upper())
+        else:
+          pdf.setFont("Helvetica-Bold", 80)
+          pdf.drawString(40, 600, lista["nome"].upper())
+
         pdf.setTitle(nome_pdf)
+
+
         pdf.setFont("Helvetica", 30)
-        pdf.drawString(100,560, 'BAUDUCO')
+        pdf.drawString(40, 560, lista["marca"].upper()+"  " +lista["gramagem"].upper())
+
+        if(len(lista["preco"].split(',')[0]) < 2):
+          pdf.setFont("Helvetica-Bold", 30)
+          pdf.drawString(110,330, 'R$')
+          pdf.setFont("Helvetica-Bold", 250)
+          pdf.drawString(150,300, lista["preco"].split(',')[0])
+          pdf.setFont("Helvetica-Bold", 100)
+          pdf.drawString(280,380, ","+lista["preco"].split(',')[1])
+        else:
+          pdf.setFont("Helvetica-Bold", 30)
+          pdf.drawString(80,330, 'R$')
+          pdf.setFont("Helvetica-Bold", 250)
+          pdf.drawString(120,300, lista["preco"].split(',')[0])
+          pdf.setFont("Helvetica-Bold", 100)
+          pdf.drawString(390,380, ","+lista["preco"].split(',')[1])
+
         pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(260,560, '300ML')
-        pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(120,380, 'R$')
-        pdf.setFont("Helvetica-Bold", 150)
-        pdf.drawString(180,360, '50')
-        pdf.setFont("Helvetica-Bold", 80)
-        pdf.drawString(350,410, ',49')
-        pdf.setFont("Helvetica-Bold", 30)
-        pdf.drawString(470,350, 'UND')
+        pdf.drawString(450,275, lista["tipo"].upper())
 
         pdf.rotate(15)
-        pdf.rect(300, 250, 250, 4, fill=True)
-        pdf.rect(340, 230, 185, 4, fill=True)
+        pdf.rect(270, 180, 250, 4, fill=True)
+        pdf.rect(310, 160, 185, 4, fill=True)
 
         pdf.save()
         print('{}.pdf criado com sucesso!'.format(nome_pdf))
     except:
         print('Erro ao gerar {}.pdf'.format(nome_pdf))
 
-lista = {'Rafaela': '19', 'Jose': '15', 'Maria': '22','Eduardo':'24'}
+listas = [
+  {"codigo": "4321", "nome": "cerveja", "marca": "12345678901234567890", "preco":"2,49", "gramagem": "300ml", "tipo": "und"},
+  {"codigo": "1234", "nome": "PÃO tradicional", "marca": "bauduco", "preco":"12,49", "gramagem": "70gr", "tipo": "und"},
+  {"codigo": "5678", "nome": "maionese", "marca": "hellmann's-pote", "preco":"22,49", "gramagem": "70gr", "tipo": "und"},
+  {"codigo": "5679", "nome": "esponja multiuso", "marca": "hellmann's-pote", "preco":"12,49", "gramagem": "70gr", "tipo": "und"}
+  ]
 
-GeneratePDF(lista)
-# sizeSpace("skol")
+for lista in listas:
+  GeneratePDFA4(lista)
+
